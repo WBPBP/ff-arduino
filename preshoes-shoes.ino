f8e1ec371fbfa4a621d4f377ba3b47134827146d
liver_is_line <51740252+dobbi030@users.noreply.github.com>
2020-05-23 16:54:37 +0900

    applied issue.

--- preshoes-shoes.ino
+++ preshoes-shoes.ino
@@ -6,8 +6,8 @@
 
 BluetoothSerial SerialBT;
 int pin[12] = {34, 32, 33, 25, 26, 27, 14, 12, 13, 4, 2, 15};
-int datas[12] = {};
-int ret[12] = {};
+int delimeter = 0xFF;
+int data_sensor = 0;
 
 
 void setup() {
@@ -17,11 +17,11 @@ void setup() {
 
 void dumpInput(void) {
   for (int i = 0; i < 12; i++) {
-    datas[i] = analogRead(pin[i]) / 256;
-    ret[i] = ((i << 4) & 0xF0) + (datas[i] & 0xF);
-    Serial.println(ret[i]);
-    SerialBT.println(ret[i]);
+    data_sensor = analogRead(pin[i]) >> 8;
+    Serial.println(data_sensor);
+    SerialBT.println(data_sensor);
   }
+  SerialBT.println(delimeter);
 }
 
 
