403e95e4181c9d0e44d081f39fae78437b8c5925
struct silver_line <dobbi030@github.com>
2020-05-31 23:03:28 +0900

    reader done

--- reader/reader.ino
+++ reader/reader.ino
@@ -1,3 +1,14 @@
+/**
+ * Reader
+ * 
+ * UART connection between reader and transmitter:
+ * 
+ * Reader     Transmitter
+ * TX <-----> RX
+ * GND <----> GND
+ * Vcc <-----> Vcc
+ */
+
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -9,26 +20,19 @@
 
 #ifdef LEFT
 #define PINS {4, 34, 26, 2, 15, 25, 13, 12, 33, 32, 27, 14}
-#define NAME "PreshoesLeft"
 #elif defined(RIGHT)
 #define PINS {25, 32, 33, 26, 27, 13, 14, 12, 4, 34, 2, 15}
-#define NAME "PreshoesRight"
 #endif
-
-
-BluetoothSerial SerialBT;
 
 int pin[12] = PINS;
 int delimeter = 0xFF;
 int data_sensor = 0;
 int biggest[12] = {0,};
 
-
 void setup() {
-  Serial.begin(9600);
-  SerialBT.begin(NAME);
+  Serial.begin(9600); // for debug
+  Serial2.begin(9600); // for transmitter
 }
-
 
 void dumpInput(void) {
   for (int i = 0; i < 12; i++) {
@@ -39,12 +43,11 @@ void dumpInput(void) {
 
     if (biggest[i] != 0)
       data_sensor = ((double)data_sensor / (double)biggest[i]) * 15;
-      
-    SerialBT.write(data_sensor);
-  }
-  SerialBT.write(delimeter);
-}
 
+    Serial2.write(data_sensor);
+  }
+  Serial2.write(delimeter);
+}
 
 void loop() {
   dumpInput();
